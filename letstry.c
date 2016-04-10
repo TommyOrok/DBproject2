9a14aa7a47fca661235c23270edd0978c0a4f566
TommyOrok <to2240@columbia.edu>
2016-04-10 15:03:37 -0400

    Just the outline of what needs to happen

---
+++ letstry.c
@@ -0,0 +1,15 @@
+//includes 
+
+
+
+//main function
+int main(){
+	//build index and load probes into the 
+
+
+	//probed index using binary search for each node and 
+	//append range ID of match to output array
+
+
+	//write output array to stdout
+}
