c37fb7def613904fecc155fea9b6af436ccd69f4
Uzo Amuzie <ua2144@columbia.edu>
2016-04-11 22:15:25 -0400

    Random Keys and Probes

--- build.c
+++ build.c
@@ -75,9 +75,9 @@ int int32_cmp(const void *x, const void *y)
 int32_t *generate(size_t n, rand32_t *gen)
 {
 	size_t i;
-	int32_t *a = malloc(n * 4);			printf("Test #4. var n = %d\n", n);
+	int32_t *a = malloc(n * 4);	
 	for (i = 0 ; i != n ; ++i)
-		a[i] = rand32_next(gen);		printf("Test #5\n");
+		a[i] = rand32_next(gen);
 	return a;
 }
 
@@ -98,7 +98,7 @@ int32_t *generate_sorted_unique(size_t n, rand32_t *gen)
 	int32_t *a = NULL;
 	do {
 		free(a);
-		m *= 1.1;							printf("Test #3\n");
+		m *= 1.1;
 		a = generate(m, gen);
 		qsort(a, m, 4, int32_cmp);		
 	} while (compact(a, m) < n);
@@ -117,60 +117,71 @@ int main(int argc, char **argv)
 		return help();
 
 
-	printf("Test #1\n");
-
 
 	// GET nunber of keys and probes
-	int keys = atoi(argv[1]);
-	int probes = atoi(argv[2]);			printf("Test #2\n");
+	int keys = atoi(argv[1]);		printf("Number of Keys: %d\n", keys);
+	int probes = atoi(argv[2]);		printf("Number of Probes: %d\n", probes);
+
 
 
 	// GENERATE random keys
 	rand32_t *gen = rand32_init(time(NULL));
-	size_t pos, n = 20;
-	int32_t *a = generate_sorted_unique(n, gen);
+	size_t pos, n = keys;									// pass number of keys from cmd line
+	int32_t *key_arr = generate_sorted_unique(n, gen);		printf("Random Keys\n");
 	for (pos = 0 ; pos != n ; ++pos) {
-		printf("%d\n", a[pos]);
+		printf("%d\n", key_arr[pos]);
 	}
 
 
-	// FREE allocated memory
-	free(a);
-	free(gen);
+	// GENERATE random probes
+	rand32_t *genn = rand32_init(time(NULL));
+	pos = 0; n = probes;									// pass number of probes from cmd line
+	int32_t *probe_arr = generate(n, genn);					printf("Random Probes\n");
+	for (pos = 0 ; pos != n ; ++pos) {
+		printf("%d\n", probe_arr[pos]);
+	}
 
 
-	// make an array of the fanouts for each level
-	int size = (sizeof(argv)/sizeof(int)) - 3;
-	int fanout_levels[size];
-
+	// MAKE an array of the fanouts for each level
+	int size = (argc - 3);	// subtracting non-fanout args
+	int *fanout_arr = malloc(sizeof(int) * size);
 	int i, j = 0;
 
-/*
-	for (i = 3; i < (sizeof(argv)/sizeof(int)); i++) {
-		fanout_levels[j] = atoi(argv[i]);
-		j++;
+	for (i = 3; i < argc; i++) {
+		// add fanout vals from argv to fanout_arr
+		fanout_arr[j] = atoi(argv[i]);	j++;
+		printf("Level %d Fanout: %d\n", j, fanout_arr[j-1]);
 	}
+	
+
+
+	// GET dimensions (length and width) of 2D array
+	//int length_tree = sizeof(fanout_arr) / sizeof(int);
+	int length_tree = argc - 3;
+	int width_tree = fanout_arr[0];
+
+	int in = 0, foo = 0;
+	for (foo = 1; foo < length_tree; foo++ ) {		
+		width_tree = width_tree * fanout_arr[in];
+	}
+
+
+	printf("Length of 2D array: %d\n", length_tree);
+	printf("Width of 2D array: %d\n", width_tree);
+
+
+/*
+	// initialize 2D array for search tree
+	int tree[length_tree][width_tree];
 */
 
 
-	// get dimensions (length and width) of 2D array
-	int length_tree = sizeof(fanout_levels) / sizeof(int);
-	int width_tree = fanout_levels[0];
-
-	int in, foo = 0;
-	for (foo = 1; foo < length_tree; foo++ ) {
-		width_tree = width_tree * fanout_levels[in];
-	}
-
-
-	// initialize 2D array for search tree
-	int tree[length_tree][width_tree];
-
-
-
 	// FREE allocated memory
-	// free(a);
-	// free(gen);
+	free(key_arr);
+	free(gen);
+	free(probe_arr);
+	free(genn);
+	free(fanout_arr);
 
 
 
